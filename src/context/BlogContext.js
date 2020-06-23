7bda9572174768b9baf84ac3659bf4ad6ad6da8a
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-23 11:43:35 +0530

    Created blogReduce function and also imported useReducer into BlogContext.js.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 const BlogContext = React.createContext();
+
+const blogReducer = (state, action) => {
+    switch action.type {
+        case "add_blogPost":
+            return [...state, { title: `Blog Post #${state.length + 1}` }];
+
+        default:
+            return state;
+    }
+}
 
 export const BlogProvider = ({ children }) => {
 
