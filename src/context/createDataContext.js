98356a5ade0323d6498b32cbc433e8afc1ec3802
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-23 13:34:41 +0530

    Wrapped state inside an object inside the Provider value.

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -6,7 +6,7 @@ export default (reducer, actions, initialState) => {
     const Provider = ({ children }) => {
         const [state, dispatch] = useReducer(reducer, initialState);
 
-        return <Context.Provider value={state}>{children}</Context.Provider>
+        return <Context.Provider value={{state}}>{children}</Context.Provider>
     }
 
     return { Context, Provider };
