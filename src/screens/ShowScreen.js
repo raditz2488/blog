1ed49c97a25d166e870f28d04c6995b3665f16bb
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-25 18:02:42 +0530

    Added useContext to get state to ShowScreen. Also added code to get blogPost for the required id from the state and id from navigation params in the ShowScreen.

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -3,6 +3,10 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const ShowScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+
+    const blogPost = state.find(blogPost => blogPost.id === navigation.getParam('id'));
+
     return <View>
         <Text>Show Screen</Text>
     </View>
