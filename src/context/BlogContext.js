94b53d9695a8874c81782fd5d48d5f84bc57d8ae
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-25 16:34:26 +0530

    Created a deleteBlogPost function.
    Update:
    1. Created a deleteBlogPost function.
    2. This funtion returns a function that takes in a id as parameter.
    3. The returned function calls dispatch with type of 'delete_blogPost' and payload of id.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,7 +7,7 @@ const blogReducer = (state, action) => {
             return [...state, { title: `Blog Post #${state.length + 1}`, id: Math.floor(Math.random() * 99999) }];
 
         case "delete_blogPost":
-            return state.filter( blogPost => blogPost.id === action.payload)
+            return state.filter( blogPost => blogPost.id !== action.payload)
 
         default:
             return state;
@@ -19,6 +19,12 @@ const addBlogPost = (dispatch) => {
     return () => {
         dispatch({ type: "add_blogPost" });
     };
+}
+
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogPost', payload: id })
+    }
 }
 
 export const { Context, Provider } = createDataContext(
