55cc4fd64b31cda33dc6d4e4a50e08e504533bf7
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-25 19:41:27 +0530

    Imported useContext hook to the CreateScreen.

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const CreateScreen = () => {
