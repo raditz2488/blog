030048bf87f0d1776d5f7d8bb5f48bb0bf0233d5
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-25 19:24:23 +0530

    The blogReducer now uses payload to set title and content of new blog for the add_blogPost action type.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,7 +4,7 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
     switch (action.type) {
         case "add_blogPost":
-            return [...state, { title: `Blog Post #${state.length + 1}`, id: Math.floor(Math.random() * 99999) }];
+            return [...state, { title: action.payload.title, content: action.payload.content, id: Math.floor(Math.random() * 99999) }];
 
         case "delete_blogPost":
             return state.filter( blogPost => blogPost.id !== action.payload)
