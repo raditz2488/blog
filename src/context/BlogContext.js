22b00bfa8f93a0bcc405d2ea93d5e66b294a42e9
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-26 16:39:11 +0530

    Created action edit_blogPost in the blogReducer and also added it in the call to createDataContext inside BlogContext.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,7 +7,12 @@ const blogReducer = (state, action) => {
             return [...state, { title: action.payload.title, content: action.payload.content, id: Math.floor(Math.random() * 99999) }];
 
         case "delete_blogPost":
-            return state.filter( blogPost => blogPost.id !== action.payload)
+            return state.filter( blogPost => blogPost.id !== action.payload )
+
+        case "edit_blogPost":
+            return state.map((blogPost) => { 
+                return blogPost.id === payload.id ? {id: payload.id, title: payload.title, content: payload.content } : blogPost
+             });
 
         default:
             return state;
@@ -38,6 +43,6 @@ const editBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(
     blogReducer, 
-    { addBlogPost, deleteBlogPost }, 
+    { addBlogPost, deleteBlogPost, editBlogPost }, 
     [{title: "Test", content: "Content", id: 1}]
 );
