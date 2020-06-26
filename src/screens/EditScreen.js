3ae1cd69098fc1e205f0672763ab04ff40eb219b
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-26 16:17:01 +0530

    Edit Screen now displays the note title and content.
    Update:
    1. Imports useContext and BlogContext.
    2. Takes in navigation parameter.
    3. Gets id param.
    4. Gets state from the Context.
    5. Finds blogPost using the id.
    6. Passes initialValues of title and content to the BlogPostForm.

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import BlogPostForm from '../components/BlogPostForm';
+import { Context } from '../context/BlogContext';
 
-const EditScreen = () => {
-    return <BlogPostForm></BlogPostForm>
+const EditScreen = ({ navigation }) => {
+    const id = navigation.getParam('id')
+    const { state } = useContext(Context);
+    const blogPost = state.find((blogPost) => blogPost.id === id)
+
+    return <BlogPostForm initialValues={ { title: blogPost.title, content: blogPost.content } }></BlogPostForm>
 }
 
 const styles = StyleSheet.create({});
